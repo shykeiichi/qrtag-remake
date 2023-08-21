4622819c278fd3e4433f552b569fa49ac0a11e10
shykeiichi <imdark200@gmail.com>
2023-08-21 22:23:19 +0200

    Home page info update

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,16 +12,17 @@
                     QRTag är en digital version av leken "Killer Game" och ett bra sätt att möta andra elever på skolan.
                 </p>
                 <p class="lead mb-4">
-                    Leken går ut på att alla har har ett mål som man ska "tagga". Du taggar ditt mål genom att lägga handen på dens axel och säga "tag". Sedan ska du scanna personens QR-kod för att få poäng. När du taggar någon så ärver du den personens mål medans ditt mål är ute ur spelet.
+                    Leken går ut på att alla har har ett mål som man ska kulla. Du kullar ditt mål genom att lägga handen på dens axel och säga kull. Sedan ska du scanna personens QR-kod för att få poäng. När du taggar någon så ärver du den personens mål medans ditt mål är ute ur spelet.
                 </p>
                 <h3 class="display-5 fw-bold text-body-emphasis">Regler</h3>
                 <ul class="text-start">
                     <li class="lead text-start">Du får inte använda våld</li>
                     <li class="lead text-start">Du får endast tagga någon som inte är på lunch eller lektion</li>
+                    <li class="lead text-start">Försök att inte ko-vakta en lektionssal där ditt mål är. Det är helt ärligt skit tråkigt</li>
                     <li class="lead text-start">Du får använda vilken metod som helst för att hitta ditt mål</li>
                 </ul>
                 <h3 class="display-5 fw-bold text-body-emphasis">Priset</h3>
-                <p class="lead mb-4">Personen som klarar sig längst och personen som taggar flest mål vinner. Priset består av heder och ett SSIS-starterpack som kommer att hjälpa dig genom terminen.</p>
+                <p class="lead mb-4">Personen som klarar sig längst och personen som taggar flest mål vinner. Priset består av heder och lite SSIS merch från elevrådet.</p>
             </div>
         </div>
     </div>
